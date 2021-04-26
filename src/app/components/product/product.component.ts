0be193c0c16c4c6cecad4638984da9d3dc4f3261
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-26 18:24:19 +0300

    mar17 commit 3

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -14,7 +15,10 @@ export class ProductComponent implements OnInit {
   dataLoaded: boolean = false;
   filterText = "";
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+     private activatedRoute: ActivatedRoute,
+     private toastrService:ToastrService
+     ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -38,5 +42,9 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi",product.productName)
   }
 }
